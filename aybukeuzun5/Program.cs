d0c911eb7bd79f466aa13cb16fb0ba7d15ee04ca
aybukeuzun <a.uzun200900@hotmail.com>
2023-05-04 18:48:06 +0300

    week5

---
+++ aybukeuzun5/Program.cs
@@ -0,0 +1,25 @@
+﻿string username, password;
+
+Console.WriteLine("enter your username:");
+username = Console.ReadLine();
+Console.WriteLine("enter your password:");
+password = Console.ReadLine();
+
+//if (username == "admin" && password == "admin1234")
+//{
+//    Console.WriteLine("welcome");
+//}
+//else
+//{
+//    Console.WriteLine("invalid data");
+//}
+
+
+if (username != "admin" |   | password != "admin1234")
+{
+    Console.WriteLine("invalid");
+}
+else
+{
+    Console.WriteLine("welcome");
+}
---
+++ aybukeuzun55/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("enter the current temp:");
+int currentTemp = Int32.Parse(Console.ReadLine());
+
+if (currentTemp <= 0)
+{
+    Console.WriteLine("freezing");
+}
+else if (currentTemp > 0 && currentTemp < 5)
+{
+    Console.WriteLine("cold");
+}
+else if (currentTemp >= 5 && currentTemp < 10)
+{
+    Console.WriteLine("chilly");
+}
+else if (currentTemp >= 10 && currentTemp < 15)
+{
+    Console.WriteLine("cool");
+}
+else if (currentTemp >= 15 && currentTemp < 25)
+{
+    Console.WriteLine("warm");
+}
+else if (currentTemp >= 25)
+{
+    Console.WriteLine("boiling hot");
+}
